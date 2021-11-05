dbd4abbdd4d49bf00210919612ca0dbc231c88f8
WILLIAM CHOONG <67597772+CWilliam001@users.noreply.github.com>
2021-11-05 23:05:10 +0800

    Second time commit

---
+++ Fundtasia/Fundtasia/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fundtasia.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: https://localhost:44384/Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
